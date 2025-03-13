048d0ae7aba467a220621a770dadd006ac426c24
Filpus <fil-pus@wp.pl>
2025-03-13 18:49:30 +0100

    Definicja Eventów

---
+++ Domain2/ZapisPostaciEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain1
+{
+    public class ZapisPostaciEvent
+    {
+        public Guid EventId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Data { get; set; }
+    }
+}
---
+++ Domain5/UdanyAtakEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain3
+{
+    public class UdanyAtakEvent
+    {
+        public Guid EventId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Data { get; set; }    
+    }
+}
